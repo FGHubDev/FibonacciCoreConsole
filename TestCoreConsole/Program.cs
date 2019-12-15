a16ee906024e061f620198bf3e5bb66984cc9a6b
Frédéric Golleau <fgolleau@hotmail.fr>
2019-12-15 04:40:01 +0100

    Suppression du code des logs.

--- TestCoreConsole/Program.cs
+++ TestCoreConsole/Program.cs
@@ -7,24 +7,18 @@ namespace TestCoreConsole
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-
             Console.WriteLine("Saisissez un nombre entre 1 et 100 :");
             Console.WriteLine(Fibonacci(Console.ReadLine()));
         }
 
         public static string Fibonacci(string stringNumber)
         {
-            //log.Info("Fibonacci - Valeur saisie : " + chaineN);
-
             try
             {
                 int n = int.Parse(stringNumber);
 
                 if (n < 1 || n > 100)
                 {
-                    //log.Info("Fibonacci - Valeur retournée : -1");
-
                     return "-1";
                 }
                 else
@@ -39,8 +33,6 @@ namespace TestCoreConsole
                         Fib[i] = Fib[i - 1] + Fib[i - 2];
                     }
 
-                    //log.Info("Fibonacci - Valeur retournée : " + Fib[n].ToString());
-
                     return Fib[n].ToString();
                 }
             }
@@ -48,14 +40,10 @@ namespace TestCoreConsole
             {
                 if (stringNumber == "")
                 {
-                    //log.Error("Fibonacci - Aucune valeur saisie.", e);
-
                     throw new ArgumentNullException("Aucune valeur saisie.", e);
                 }
                 else
                 {
-                    //log.Error("Fibonacci - Valeur incorrecte. Valeur saisie : " + chaineN, e);
-
                     throw new Exception("Valeur incorrecte.", e);
                 }
             }
